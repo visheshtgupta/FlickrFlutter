54f33e80fae1411e8022de4f97c553023de82efa
visheshtgupta <vishesht.gupta@mutualmobile.com>
2022-09-28 23:22:43 +0530

    footer Loader

--- lib/api/api.service.dart
+++ lib/api/api.service.dart
@@ -1,17 +1,27 @@
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  Future<String> fetchAlbum(text) async {
+  Future<String> fetchAlbum(text, loadMore, loadedPages) async {
     var httpsUri = Uri(
-    scheme: 'https',
-    host: 'api.flickr.com',
-    path: '/services/rest',
-    queryParameters: {'method':'flickr.photos.search','api_key':'23afa6d878afc63b94f925fe662bee71','format':'json','license':'4','nojsoncallback':'1','text': text, 'sort': 'interestingness-desc','per_page':'10','page':'1'});
+        scheme: 'https',
+        host: 'api.flickr.com',
+        path: '/services/rest',
+        queryParameters: {
+          'method': 'flickr.photos.search',
+          'api_key': '23afa6d878afc63b94f925fe662bee71',
+          'format': 'json',
+          'license': '4',
+          'nojsoncallback': '1',
+          'text': text,
+          'sort': 'interestingness-desc',
+          'per_page': '10',
+          'page': loadMore ? '$loadedPages' : '1',
+        });
 
     var response = await http.get(httpsUri);
     if (response.statusCode == 200) {
       return response.body;
-    }else {
+    } else {
       throw Exception('Failed to load album');
     }
   }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,101 +15,127 @@ class _HomeState extends State<Home> {
   late List<dynamic> dataResponse;
   TextEditingController _textController = TextEditingController();
   String texts = '';
+  int loadedPages = 1;
+  bool loader = false;
+  ScrollController _scrollController =
+      ScrollController(initialScrollOffset: 5.0);
 
   @override
   void initState() {
     super.initState();
-    getApi('');
+    _scrollController.addListener(_scrollListener);
+    dataResponse = [];
   }
 
-  getApi(text) {
-     ApiService().fetchAlbum(text).then((value) => {
-       setState(() {
-         mapResponse = jsonDecode(value) ;
-         dataResponse = mapResponse['photos']['photo'];
-       })
+  getApi(text, loadMore, loadedPages) {
+    setState(() {
+      loader = !loader;
     });
+    ApiService().fetchAlbum(text, loadMore, loadedPages).then((value) => {
+          setState(() {
+            mapResponse = jsonDecode(value);
+            if (loadMore) {
+              dataResponse = dataResponse + mapResponse['photos']['photo'];
+              loadedPages++;
+            } else {
+              dataResponse = mapResponse['photos']['photo'];
+            }
+            loader = !loader;
+          })
+        });
+  }
+
+  _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      getApi(texts, true, loadedPages);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Flickr'),
+      appBar: AppBar(
+        title: Text('Flickr'),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: const [
+            UserAccountsDrawerHeader(
+              accountName: Text('Vishesht'),
+              accountEmail: Text("gvishesht@gmail.com"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Free-Image.png"),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.person),
+              title: Text('Account'),
+              subtitle: Text("Personal"),
+              trailing: Icon(Icons.edit),
+            ),
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text('Email'),
+              subtitle: Text("gvishesht@gmail.com"),
+              trailing: Icon(Icons.send),
+            ),
+          ],
         ),
-        drawer: Drawer(
-          child: ListView(
-            children: const [
-              UserAccountsDrawerHeader(
-                accountName: Text('Vishesht'),
-                accountEmail: Text("gvishesht@gmail.com"),
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Free-Image.png"),
-                ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.only(top: 12.0),
+            child: TextField(
+              controller: _textController,
+              onChanged: (text) {
+                setState(() {
+                  texts = text;
+                  getApi(texts, false, loadedPages);
+                });
+              },
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Search Image',
               ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text('Account'),
-                subtitle: Text("Personal"),
-                trailing: Icon(Icons.edit),
-              ),
-              ListTile(
-                leading: Icon(Icons.email),
-                title: Text('Email'),
-                subtitle: Text("gvishesht@gmail.com"),
-                trailing: Icon(Icons.send),
-              ),
-            ],
+            ),
           ),
-        ),
-        body: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 12.0),
-                child: TextField(
-                  controller: _textController,
-                  onChanged: (text){
-                    setState(() {
-                      texts = text;
-                      getApi(texts);
-                    });
-                  },
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Search Image',
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 10,
-              ),
-              FutureBuilder<String>(
-                future: ApiService().fetchAlbum('car'), // async work
-                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                  switch (snapshot.connectionState) {
-                    case ConnectionState.waiting: return Text('Loading....');
-                    default:
-                      if (snapshot.hasError)
-                        return Text('Error: ${snapshot.error}');
-                      else if(snapshot.hasData){
-                        return Flexible(child: GridView.count(
-                            crossAxisCount: 2,
-                            children: List.generate(dataResponse.length, (index) {
-                              var Url = 'https://live.staticflickr.com/${dataResponse[index]['server']}/${dataResponse[index]['id']}_${dataResponse[index]['secret']}_m.jpg';
-                              return Padding(padding: EdgeInsetsDirectional.all(4),
+          const SizedBox(
+            height: 10,
+          ),
+          FutureBuilder<String>(
+            future: ApiService()
+                .fetchAlbum('car', false, loadedPages), // async work
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasError)
+                return Text('Error: ${snapshot.error}');
+              else if (snapshot.hasData) {
+                return Flexible(
+                    child: GridView.count(
+                        controller: _scrollController,
+                        physics: BouncingScrollPhysics(),
+                        crossAxisCount: 2,
+                        children: List.generate(dataResponse.length, (index) {
+                          var Url =
+                              'https://live.staticflickr.com/${dataResponse[index]['server']}/${dataResponse[index]['id']}_${dataResponse[index]['secret']}_m.jpg';
+                          return Padding(
+                              padding: EdgeInsetsDirectional.all(4),
                               child: Image.network(Url));
-                            }
-                            )
-                        )  );
-                      }else{
-                        return Text('Expection error');
-                      }
-                  }
-                },
-              ),
-            ],
+                        })));
+              } else {
+                return Text(' ');
+              }
+            },
           ),
-        );
+          if (loader)
+            CircularProgressIndicator(
+              strokeWidth: 3.0,
+            )
+        ],
+      ),
+    );
   }
 }
